c68bb543d670873e4416de3728bb9cd00cca4953
Manohargov <manoaad@gmail.com>
2025-05-16 19:48:34 +0530

    requirments added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="InfoRetrievalGenAI",
+    version="0.0.0",
+    author="Your Name",
+    author_email="your@email.com",
+    packages=find_packages(),
+    install_requires=[],
+)
