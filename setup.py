5d93090b8b8cc06304d4204cc3b03810c09a2fd4
Manohargov <manoaad@gmail.com>
2025-05-19 10:27:26 +0530

    set up updated

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name="InfoRetrievalGenAI",
     version="0.0.0",
-    author="Your Name",
-    author_email="your@email.com",
+    author="Manohar",
+    author_email="manoaad@gmail.com",
     packages=find_packages(),
     install_requires=[],
 )
